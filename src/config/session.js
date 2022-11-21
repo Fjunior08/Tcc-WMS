d50133a62828450be7e5f0ddd8e70006623fee26
Francisco Jr <juniorlina872@gmail.com>
2022-11-21 20:13:25 -0300

    Initial commit

---
+++ src/config/session.js
@@ -0,0 +1,19 @@
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+
+
+module.exports = session({
+  store: new MongoDBStore({
+    uri: "mongodb://localhost:27017/",
+    databaseName: "cms",
+    collection: "mySessions",
+  }),
+  secret: "secretsessiontest",
+  resave: false,
+  saveUninitializad: false,
+  cookie: {
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  },
+});
+
+// console.log(process.env.DB_URL);
